f6f5f45f896bb0f342ada832415a0bde27382bd5
Michael Korolev <mkorole@sfdev.com>
2013-06-20 16:11:09 +0300

    try add crossdomain requests; use hash

--- for-test/server.js
+++ for-test/server.js
@@ -4,6 +4,23 @@ var url = require("url");
 function onRequest(request, response) {
   var parseurl = url.parse(request.url);
 
+   var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '<script> var timeId = setTimeout(funcion(){
+		var el = document.getElementById("content");
+		el.innerHtml("www qqq eee");
+		location.hash = "megaresponse!!"
+    },3000);</script>'+
+    '</head>'+
+    '<body>'+
+    '<h1>From <script>document.write(window.location.hostname)</script></h1>'+
+    '<div id="content"></div>'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
   console.log("Request received for: "+parseurl.query);
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World "+parseurl.pathname+" query"+parseurl.query);
